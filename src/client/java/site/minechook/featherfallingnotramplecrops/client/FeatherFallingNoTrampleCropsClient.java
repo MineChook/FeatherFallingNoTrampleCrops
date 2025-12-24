a6f173bbafb068408611107ceda667d03c8c2020
MineChook <thomas.cobiac@outlook.com>
2025-12-24 11:41:52 +1030

    First Commit

---
+++ src/client/java/site/minechook/featherfallingnotramplecrops/client/FeatherFallingNoTrampleCropsClient.java
@@ -0,0 +1,10 @@
+package site.minechook.featherfallingnotramplecrops.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FeatherFallingNoTrampleCropsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/site/minechook/featherfallingnotramplecrops/mixins/FarmlandBlockMixin.java
@@ -0,0 +1,41 @@
+package site.minechook.featherfallingnotramplecrops.mixins;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FarmlandBlock.class)
+abstract class FarmlandBlockMixin {
+
+    private static boolean hasEnchantment(ItemStack stack, RegistryKey<Enchantment> enchantment) {
+        return stack.getEnchantments().getEnchantments().toString().
+                contains(enchantment.getValue().toString());
+    }
+
+    @Inject(method = "onLandedUpon", at = @At("HEAD"), cancellable = true)
+    private void onJump(World world, BlockState state, BlockPos pos, Entity entity, double fallDistance, CallbackInfo ci) {
+
+        if (!(entity instanceof PlayerEntity player)) {
+            return;
+        }
+        ItemStack boots = player.getEquippedStack(EquipmentSlot.FEET);
+        if (boots.isEmpty()) {
+            return;
+        }
+        if (hasEnchantment(boots, Enchantments.FEATHER_FALLING)) {
+            ci.cancel();
+        }
+    }
+}
